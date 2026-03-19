02d397030d32c3424452d598457d0292aa0557d5
Taratata37 <julien.amouriq@zaclys.net>
2026-03-19 22:02:29 +0100

    Masquage du menu Admin aux non-admins

--- common_header.sql
+++ common_header.sql
@@ -1,10 +1,14 @@
-select 
-    'shell'                     as component,
-    'GTA - Gestion des Tracasseries Administratives' as title,
-    'database'                  as icon,
-    '/'                         as link,
-	JSON('{"title":"Accueil","icon":"home","link":"/"}') as menu_item,
-    JSON('{"title":"Gestion","icon":"hexagons","submenu":[{"link":"/gestion/inscription.sql","title":"Inscription","icon":"edit"},{"link":"/gestion/presence.sql","title":"Feuille de présence","icon":"file-text"},{"link":"/gestion/formalites_manquantes.sql","title":"Formalités manquantes","icon":"file-report"},{"link":"/gestion/participants.sql","title":"Participants","icon":"file-text"},{"link":"/gestion/equipes.sql","title":"Équipes","icon":"users-group"}]}') as menu_item,
-        JSON('{"title":"Administration","icon":"tool","submenu":[{"link":"/admin/formalites.sql","title":"Modifier les formalités","icon":"book"},{"link":"/admin/sacrements.sql","title":"Modifier les sacrements","icon":"list-details"},{"link":"/admin/sections.sql","title":"Modifier les sections","icon":"list-details"},{"link":"/admin/def_mdp.sql","title":"Définir mdp","icon":"key"},{"link":"/admin/rgpd.sql","title":"Purge RGPD","icon":"user-off"}]}') as menu_item,
-    'boxed'                     as layout,
-    'fr-FR'                     as language;
+SELECT 
+    'shell'                     AS component,
+    'GTA - Gestion des Tracasseries Administratives' AS title,
+    'database'                  AS icon,
+    '/'                         AS link,
+    JSON('{"title":"Accueil","icon":"home","link":"/"}') AS menu_item,
+    JSON('{"title":"Gestion","icon":"hexagons","submenu":[{"link":"/gestion/inscription.sql","title":"Inscription","icon":"edit"},{"link":"/gestion/presence.sql","title":"Feuille de présence","icon":"file-text"},{"link":"/gestion/formalites_manquantes.sql","title":"Formalités manquantes","icon":"file-report"},{"link":"/gestion/participants.sql","title":"Participants","icon":"file-text"},{"link":"/gestion/equipes.sql","title":"Équipes","icon":"users-group"}]}') AS menu_item,
+    CASE WHEN scn.IdDoyenne IS NULL THEN
+        JSON('{"title":"Administration","icon":"tool","submenu":[{"link":"/admin/formalites.sql","title":"Modifier les formalités","icon":"book"},{"link":"/admin/sacrements.sql","title":"Modifier les sacrements","icon":"list-details"},{"link":"/admin/sections.sql","title":"Modifier les sections","icon":"list-details"},{"link":"/admin/def_mdp.sql","title":"Définir mdp","icon":"key"},{"link":"/admin/rgpd.sql","title":"Purge RGPD","icon":"user-off"}]}')
+    END                         AS menu_item,
+    'boxed'                     AS layout,
+    'fr-FR'                     AS language
+FROM v_sessions_valides scn
+WHERE scn.jeton = sqlpage.cookie('jeton_session');
